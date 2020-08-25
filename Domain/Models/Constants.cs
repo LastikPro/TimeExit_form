2925d971186dcdb7d909da376789ea75d338660b
Богдан <rekrut020@gmail.com>
2020-08-25 16:53:46 +0300

    /

---
+++ Domain/Models/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models
+{
+    public static class Constants
+    {
+#if DEBUG
+        public const string DefaultConnection = "Server = PC1; Database=Start;User ID = sa; Password=connect;Integrated Security = True";
+#else
+        public const string DefaultConnection =
+            "Server=SP-VL;Database=Start01Bd;Persist Security Info=True;User ID=xxxx;Password=xxxxxx";
+#endif
+    }
+}
---
+++ WebUi/ClientApp/src/api-authorization/authorize.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthorizeService } from './authorize.service';
+import { tap } from 'rxjs/operators';
+import { ApplicationPaths, QueryParameterNames } from './api-authorization.constants';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizeGuard implements CanActivate {
+  constructor(private authorize: AuthorizeService, private router: Router) {
+  }
+  canActivate(
+    _next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      return this.authorize.isAuthenticated()
+        .pipe(tap(isAuthenticated => this.handleAuthorization(isAuthenticated)));
+  }
+
+  private handleAuthorization(isAuthenticated: boolean) {
+    if (!isAuthenticated) {
+      this.router.navigateByUrl("authentication/login?returnUrl=%2Fauthentication%2Flogin")
+    }
+  }
+}
